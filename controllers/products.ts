0e9118f8d7159446287a9fd7d9081f81cdd56347
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-07-19 00:02:46 +0530

    getproduct and addproduct added

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,3 +1,4 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import Product from "../types.ts";
 
 let products: Product[] = [
@@ -30,4 +31,62 @@ const getProducts = ({ response }: { response: any }) => {
   };
 };
 
-export { getProducts };
+// @desc Get single product
+// @route GET /api/v1/products/:id
+const getProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+  if (product) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      message: "No product found",
+    };
+  }
+};
+
+// @desc Add product
+// @route POST /api/v1/products
+const addProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = request.body();
+
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      message: "No data",
+    };
+  } else {
+    const product: Product = await body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
+};
+
+// @desc Update product
+// @route PUT /api/v1/products/:id
+const updateProduct = ({ response }: { response: any }) => {
+  response.body = "update";
+};
+
+// @desc Delete product
+// @route DELETE /api/v1/products/:id
+const deleteProduct = ({ response }: { response: any }) => {
+  response.body = "delete";
+};
+
+export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };

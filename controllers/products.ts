34002a88dbfff63a11417bde26f6f78c3dc47ee3
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-07-19 00:11:58 +0530

    update & delete product api added

--- controllers/products.ts
+++ controllers/products.ts
@@ -79,14 +79,49 @@ const addProduct = async (
 
 // @desc Update product
 // @route PUT /api/v1/products/:id
-const updateProduct = ({ response }: { response: any }) => {
-  response.body = "update";
+const updateProduct = async (
+  { params, request, response }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+  if (product) {
+    const body = request.body();
+    const updateData: { name?: string; description?: string; price?: number } =
+      await body.value;
+
+    products = products.map((p) =>
+      p.id === params.id ? { ...p, ...updateData } : p
+    );
+
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: products,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      message: "No product found",
+    };
+  }
 };
 
 // @desc Delete product
 // @route DELETE /api/v1/products/:id
-const deleteProduct = ({ response }: { response: any }) => {
-  response.body = "delete";
+const deleteProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  products = products.filter((p) => p.id !== params.id);
+
+  response.status = 200;
+  response.body = {
+    success: true,
+    data: products,
+  };
 };
 
 export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };

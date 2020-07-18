9eb4d39e9629b35365f4de22e1e41bbe80b47c83
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-07-19 00:30:40 +0530

    added denon

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
 
-const port = 5000;
+const port = Deno.env.get("PORT") || 5000;
 
 const app = new Application();
 
@@ -10,4 +10,4 @@ app.use(router.allowedMethods());
 
 console.log(`server running on port ${port}`);
 
-await app.listen({ port });
+await app.listen({ port: +port });
